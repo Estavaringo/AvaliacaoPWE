c520cca746e2e9851a994a0f3a7a7be14347e2a8
flaviosampaioreisdelima <flaviosampaioreisdelima@192.168.1.100>
2016-04-17 16:07:02 -0300

    Correções no Filtro

--- src/java/ifsp/pwe/prova/servlet/NovaCorrecao.java
+++ src/java/ifsp/pwe/prova/servlet/NovaCorrecao.java
@@ -6,7 +6,11 @@
 package ifsp.pwe.prova.servlet;
 
 import ifsp.pwe.prova.beans.Correcao;
+import ifsp.pwe.prova.beans.Usuario;
 import ifsp.pwe.prova.dao.CorrecaoDAO;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -22,7 +26,7 @@ public class NovaCorrecao implements Tarefa{
         
         //grava as informações da atividade no objeto
         correcao.setComentario(req.getParameter("titulo"));
-        correcao.setIdAtividade(req.getParameter("atividade"));
+        correcao.setIdAtividade(Integer.parseInt(req.getParameter("atividade")));
         
         //armazena o id do usuario para aquela atividade
         HttpSession session = req.getSession();
@@ -38,7 +42,7 @@ public class NovaCorrecao implements Tarefa{
 
     @Override
     public boolean verifica() {
-        
+        return true;
     }
     
 }
