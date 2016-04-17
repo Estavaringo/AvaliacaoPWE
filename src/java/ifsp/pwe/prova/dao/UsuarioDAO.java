9dc1d8032e4109be1be2c78dde8b8f7490de13d0
flaviosampaioreisdelima <flaviosampaioreisdelima@192.168.1.100>
2016-04-17 14:11:10 -0300

    Métodos de consulta do Usuário implementado com sucesso.

--- src/java/ifsp/pwe/prova/dao/UsuarioDAO.java
+++ src/java/ifsp/pwe/prova/dao/UsuarioDAO.java
@@ -15,25 +15,34 @@ import java.sql.SQLException;
  * @author gabri
  */
 public class UsuarioDAO {
-
+    
+    BancoDados bd = new BancoDados();
+    
     public void adiciona(Usuario usuario) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    public Usuario buscaPorEmailESenha(String email, String senha) {
+    public Usuario buscaPorEmailESenha(String email, String senha) throws SQLException{
         try {
             Usuario obj = null;
             bd.conectar();
-            String strSQL = "SELECT admin, email, senha FROM usuario WHERE email = ? AND senha = ?";
+            String strSQL = "SELECT USUA_NM, USUA_DT, USUA_ENDE, USUA_CORRETOR FROM USUARIO WHERE USUA_EMAIL = ? AND USUA_SENHA = ?";
             PreparedStatement p = bd.connection.prepareStatement(strSQL);
             p.setString(1, email);
             p.setString(2, senha);
             ResultSet rs = p.executeQuery();
             if (rs.next()) {
                 obj = new Usuario();
-                obj.setAdmin(rs.getInt("admin"));
-                obj.setEmail(rs.getString("email"));
-                obj.setSenha(rs.getString("senha"));
+                obj.setNome(rs.getString("USUA_NM"));
+                obj.setNome(rs.getString("USUA_DT"));
+                obj.setNome(rs.getString("USUA_ENDE"));
+                if(rs.getString("USUA_COR").equals("1")){
+                    obj.setCorretor(true);
+                }else{
+                    obj.setCorretor(false);
+                }
+                obj.setEmail(email);
+                obj.setSenha(senha);
                 p.close();
                 bd.desconectar();
                 return obj;
