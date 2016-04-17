579935794104b4f6b3fe9d6710c070bc8cd766a9
flaviosampaioreisdelima <flaviosampaioreisdelima@192.168.1.100>
2016-04-17 16:06:35 -0300

    Correções no Filtro

--- src/java/ifsp/pwe/prova/servlet/Login.java
+++ src/java/ifsp/pwe/prova/servlet/Login.java
@@ -7,6 +7,7 @@ package ifsp.pwe.prova.servlet;
 
 import ifsp.pwe.prova.beans.Usuario;
 import ifsp.pwe.prova.dao.UsuarioDAO;
+import java.sql.SQLException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -15,20 +16,28 @@ import javax.servlet.http.HttpSession;
  *
  * @author gabri
  */
-public class Login implements Tarefa{
+public class Login implements Tarefa {
+
+    Usuario usuario;
 
     @Override
     public String executa(HttpServletRequest req, HttpServletResponse resp) {
-        String email = req.getParameter("email");
-        String senha = req.getParameter("senha");
-        Usuario usuario = null;
+        try {
+            String email = req.getParameter("email");
+            String senha = req.getParameter("senha");
+            
             usuario = new UsuarioDAO().buscaPorEmailESenha(email, senha);
-        if(usuario==null){
-            return "Invalido.html";
-        }else{
-            HttpSession session = req.getSession();
-            session.setAttribute("usuarioLogado", usuario);
-            return "index.jsp";
+            
+            if (usuario == null) {
+                return "Invalido.html";
+            } else {
+                HttpSession session = req.getSession();
+                session.setAttribute("usuarioLogado", usuario);
+                return "index.jsp";
+            }
+        } catch (SQLException ex) {
+            System.err.println("Erro ao consultar usuário no banco de dados. Detalhes: " + ex.getMessage());
+            return "Erro.html";
         }
     }
 
@@ -36,5 +45,5 @@ public class Login implements Tarefa{
     public boolean verifica() {
         return false;
     }
-    
+
 }
