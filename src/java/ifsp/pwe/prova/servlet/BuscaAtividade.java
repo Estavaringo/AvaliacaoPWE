1fe92a46fbb36f22147ecbf4e5013fb4bef88271
gabri <gabri@DESKTOP-5D52A1M.home>
2016-04-17 16:53:46 -0300

    Implementado lógica para busca de atividades

--- src/java/ifsp/pwe/prova/servlet/BuscaAtividade.java
+++ src/java/ifsp/pwe/prova/servlet/BuscaAtividade.java
@@ -5,10 +5,33 @@
  */
 package ifsp.pwe.prova.servlet;
 
+import ifsp.pwe.prova.beans.Atividade;
+import ifsp.pwe.prova.dao.AtividadeDAO;
+import java.util.Collection;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  *
  * @author gabri
  */
-public class BuscaAtividade {
-    
+public class BuscaAtividade implements Tarefa {
+
+    @Override
+    public String executa(HttpServletRequest req, HttpServletResponse resp) {
+        String filtro = req.getParameter("filtro");
+        Collection<Atividade> listaAtividade;
+
+        listaAtividade = new AtividadeDAO().buscaSimilar(filtro);
+        
+        req.setAttribute("buscaAtividade", listaAtividade);
+        
+        return "/WEB-INF/Paginas/BuscaAtividade.jsp";
+    }
+
+    @Override
+    public boolean verifica() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
