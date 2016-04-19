3830991b4ef8866782f95214c0a0650f7a3629ad
flaviosampaioreisdelima <flaviosampaioreisdelima@192.168.1.100>
2016-04-18 21:35:50 -0300

    Correções no Filtro

--- src/java/ifsp/pwe/prova/dao/CorrecaoDAO.java
+++ src/java/ifsp/pwe/prova/dao/CorrecaoDAO.java
@@ -6,6 +6,10 @@
 package ifsp.pwe.prova.dao;
 
 import ifsp.pwe.prova.beans.Correcao;
+import ifsp.pwe.prova.beans.Usuario;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  *
@@ -14,7 +18,55 @@ import ifsp.pwe.prova.beans.Correcao;
 public class CorrecaoDAO {
 
     public void adiciona(Correcao correcao) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    
+    }
+    
+    BancoDados bd = new BancoDados();
+    
+    public void insereCorrecao(){
+        
+        
+        
+    }
+    
+    
+
+    public Usuario buscaPorEmailESenha(String email, String senha) throws SQLException{
+        try {
+            Usuario obj = null;
+            bd.conectar();
+            String strSQL = "SELECT USUA_NM, USUA_EMAIL, USUA_DT, USUA_ENDE, USUA_CORRETOR FROM USUARIO WHERE USUA_EMAIL = ? AND USUA_SENHA = ?";
+            PreparedStatement p = bd.connection.prepareStatement(strSQL);
+            p.setString(1, email);
+            p.setString(2, senha);
+            ResultSet rs = p.executeQuery();
+            if (rs.next()) {
+                obj = new Usuario();
+                obj.setNome(rs.getString("USUA_NM"));
+                obj.setEmail(rs.getString("USUA_EMAIL"));
+                obj.setNome(rs.getString("USUA_DT"));
+                obj.setNome(rs.getString("USUA_ENDE"));
+                if(rs.getString("USUA_COR").equals("1")){
+                    obj.setCorretor(true);
+                }else{
+                    obj.setCorretor(false);
+                }
+                
+                p.close();
+                bd.desconectar();
+                return obj;
+            }
+            p.close();
+            bd.desconectar();
+            return obj;
+        } catch (SQLException ex) {
+            bd.desconectar();
+            throw ex;
+        }
+    }
+
+
+
     }
     
 }
