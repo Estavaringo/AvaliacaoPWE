b7e758714b76d91dd424cd36467f922e3b766e3b
flaviosampaioreisdelima <flaviosampaioreisdelima@192.168.1.100>
2016-04-25 20:47:39 -0300

    Alterações gerais no Projeto

--- src/java/ifsp/pwe/prova/servlet/BuscaAtividade.java
+++ src/java/ifsp/pwe/prova/servlet/BuscaAtividade.java
@@ -6,8 +6,11 @@
 package ifsp.pwe.prova.servlet;
 
 import ifsp.pwe.prova.beans.Atividade;
+import ifsp.pwe.prova.beans.Correcao;
 import ifsp.pwe.prova.beans.Usuario;
 import ifsp.pwe.prova.dao.AtividadeDAO;
+import ifsp.pwe.prova.dao.CorrecaoDAO;
+import ifsp.pwe.prova.dao.UsuarioDAO;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import javax.servlet.http.HttpServletRequest;
@@ -24,6 +27,10 @@ public class BuscaAtividade implements Tarefa {
     private ArrayList<Atividade> listaAtividade = null;
     private Usuario usuario = null;
     private String filtroDeTitulo = "";
+    
+    //Instância as DAO de Usuario e Correcao
+    private UsuarioDAO usuarioDAO = new UsuarioDAO();
+    private CorrecaoDAO correcaoDAO = new CorrecaoDAO();
 
     @Override
     public String executa(HttpServletRequest req, HttpServletResponse resp) {
@@ -39,6 +46,29 @@ public class BuscaAtividade implements Tarefa {
             
             //Localiza todas a atividades através do Titulo
             listaAtividade = new AtividadeDAO().buscaAtividadesPorTitulo(usuario, filtroDeTitulo);
+            
+            for (Atividade obj : listaAtividade) {
+                
+                //Através de UsuarioDAO, localiza os dados do Usuário
+                obj.setUsuario(usuarioDAO.buscaPorID(obj.getId()));
+
+                /*Se o Usuario estiver vazio, entende que ele está deslogado. Desta forma, 
+                não será atribuido os dados das Correções*/
+                if (usuario != null) {
+                    //Verifica se o usuario possui o Perfil de Corretor de Atividades.
+                    //Se o usuario for corretor, atribui todas as correções.
+                    if (usuario.isCorretor()) {
+                        //Através de CorrecaoDAO, localiza os dados da Correcao
+                        obj.setCorrecao(correcaoDAO.buscaPorIDAtividade(obj.getId()));
+                    } else 
+                        //Se o usuario não for corretor, atribui a Correcao, para as atividades 
+                        //que foram submetidas por ele
+                        if (usuario.getId() == obj.getIdUsuario()) {
+                        //Através de CorrecaoDAO, localiza os dados da Correcao
+                        obj.setCorrecao(correcaoDAO.buscaPorIDAtividade(obj.getId()));
+                    }
+                }
+            }
             
             //Atribui a lista de atividades preenchida como Atributo da Requisição 
             req.setAttribute("listaDeAtividades", listaAtividade);
