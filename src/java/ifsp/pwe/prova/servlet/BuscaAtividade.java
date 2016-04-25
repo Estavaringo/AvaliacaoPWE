e943631a5e20ac71f8519d638e4592bde41cc5fb
flaviosampaioreisdelima <flaviosampaioreisdelima@192.168.1.100>
2016-04-24 22:58:17 -0300

    Alterado o nome do atributo da requisição.

--- src/java/ifsp/pwe/prova/servlet/BuscaAtividade.java
+++ src/java/ifsp/pwe/prova/servlet/BuscaAtividade.java
@@ -9,8 +9,6 @@ import ifsp.pwe.prova.beans.Atividade;
 import ifsp.pwe.prova.dao.AtividadeDAO;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -27,12 +25,13 @@ public class BuscaAtividade implements Tarefa {
 
         try {
             listaAtividade = new AtividadeDAO().buscaPorTitulo(filtro);
-            req.setAttribute("buscaAtividade", listaAtividade);
+            req.setAttribute("listaDeAtividades", listaAtividade);
         } catch (SQLException ex) {
-            Logger.getLogger(BuscaAtividade.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Erro ao Buscar Atividade. Detalhes: " + ex.getMessage());
+            return "Erro.html";
         }
 
-        return "/WEB-INF/Paginas/BuscaAtividade.jsp";
+        return "/WEB-INF/Paginas/Atividades.jsp";
     }
 
     @Override
